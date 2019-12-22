aa40a446c466345fbba5f5547aa3ad68f4790651
marian.faryna <marian.faryna@vitech.com.ua>
2019-12-22 19:09:27 +0200

    added censorship bean

--- src/main/java/com/test/url/shortener/controller/ShortenerController.java
+++ src/main/java/com/test/url/shortener/controller/ShortenerController.java
@@ -1,5 +1,8 @@
 package com.test.url.shortener.controller;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.test.url.shortener.algorithm.ShortenAlgorithm;
 import com.test.url.shortener.validator.BannedWordValidator;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class ShortenerController {
+    private LoadingCache<String, String> urlCache;
 
     private final BannedWordValidator bannedWordValidator;
     private final ShortenAlgorithm shortenAlgorithm;
@@ -18,12 +25,26 @@ public class ShortenerController {
         this.shortenAlgorithm = shortenAlgorithm;
     }
 
+    @PostConstruct
+    public void postConstruct() {
+        urlCache = CacheBuilder
+                .newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String key)  {
+                        return shortenAlgorithm.shorten(key);
+                    }
+                });
+    }
+
     @PostMapping("/shorten")
     public String shorten(@RequestBody String fullUrl) {
 
         String shortenUrl;
         do {
-            shortenUrl = shortenAlgorithm.shorten(fullUrl);
+            shortenUrl = urlCache.getUnchecked(fullUrl);
         } while (!bannedWordValidator.validate(shortenUrl));
 
         return shortenUrl;
--- src/main/java/com/test/url/shortener/validator/BannedWordValidator.java
+++ src/main/java/com/test/url/shortener/validator/BannedWordValidator.java
@@ -2,9 +2,33 @@ package com.test.url.shortener.validator;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Component
 public class BannedWordValidator {
-    public boolean validate(String url) {
-        return true;
+    private Set<String> bannedWords;
+
+    @PostConstruct
+    public void init() {
+        try {
+            //TODO check for file availability
+            this.bannedWords = Files
+                    .lines(Paths.get(getClass().getClassLoader().getResource("fileTest.txt").toURI()))
+                    .collect(Collectors.toSet());
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean validate(final String url) {
+        final List<String> occurred = this.bannedWords.stream().filter(url::contains).collect(Collectors.toList());
+        return occurred.isEmpty();
     }
 }
